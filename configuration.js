6cbcc6ab0a4e5c4db0f34b9f8b273f211ed9894b
rrpc <denisgolubovich67gd@gmail.com>
2024-03-09 17:59:03 +0300

    1

--- configuration.js
+++ configuration.js
@@ -24,72 +24,72 @@ const lvl1 =
         ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
         ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]
     ];
-// FirstLocation =
-//     [
-//         ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "leaf", "empty"],
-//         ["empty", "empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty", "empty", "empty", "leaf", "leaf", "leaf", "leaf"],
-//         ["empty", "empty", "empty", "empty", "leaf", "leaf", "leaf", "empty", "leaf", "empty", "leaf", "leaf", "leaf", "wood", "leaf"],
-//         ["empty", "empty", "empty", "leaf", "leaf", "wood", "leaf", "leaf", "leaf", "leaf", "leaf", "leaf", "leaf", "wood", "leaf"],
-//         ["empty", "empty", "leaf", "leaf", "wood", "wood", "leaf", "leaf", "wood", "leaf", "wood", "leaf", "leaf", "wood", "empty"],
-//         ["empty", "empty", "empty", "empty", "wood", "wood", "empty", "empty", "wood", "empty", "wood", "empty", "empty", "wood", "empty"],
-//         ["empty", "empty", "empty", "empty", "wood", "wood", "empty", "empty", "wood", "empty", "wood", "empty", "empty", "wood", "empty"],
-//         ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-//         ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
-//         ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "grхуйлохпиздаааааааааааound", "ground", "ground", "ground"]
-//     ];
-// SecondLocation =    [
-//         ["empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty", "empty", "empty", "leaf", "empty", "empty", "leaf", "empty"],
-//         ["empty", "leaf", "empty", "leaf", "leaf", "leaf", "empty", "leaf", "empty", "leaf", "leaf", "leaf", "leaf", "leaf", "leaf"],
-//         ["leaf", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "wood", "leaf"],
-//         ["leaf", "wood", "leaf", "leaf", "wood", "leaf", "leaf", "wood", "leaf", "leaf", "wood", "leaf", "wood", "wood", "leaf"],
-//         ["empty", "wood", "leaf", "wood", "wood", "leaf", "leaf", "wood", "wood", "leaf", "wood", "leaf", "wood", "wood", "empty"],
-//         ["empty", "wood", "empty", "wood", "wood", "empty", "empty", "wood", "wood", "empty", "wood", "empty", "wood", "wood", "empty"],
-//         ["empty", "wood", "empty", "wood", "wood", "empty", "empty", "wood", "wood", "empty", "wood", "empty", "wood", "wood", "empty"],
-//         ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-//         ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
-//         ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]  
-//     ];
-// ThirdLocation =
-    // [
-    //     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty"],
-    //     ["empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty", "empty", "leaf", "leaf", "leaf", "leaf", "empty", "empty"],
-    //     ["empty", "empty", "leaf", "leaf", "leaf", "leaf", "empty", "leaf", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "empty"],
-    //     ["empty", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "leaf", "leaf", "wood", "leaf", "wood", "leaf", "leaf", "empty"],
-    //     ["leaf", "leaf", "wood", "leaf", "wood", "wood", "leaf", "wood", "leaf", "wood", "empty", "wood", "wood", "leaf", "leaf"],
-    //     ["empty", "empty", "wood", "empty", "wood", "wood", "empty", "wood", "empty", "wood", "empty", "wood", "wood", "empty", "empty"],
-    //     ["empty", "empty", "wood", "empty", "wood", "wood", "empty", "wood", "empty", "wood", "empty", "wood", "wood", "empty", "empty"],
-    //     ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-    //     ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
-    //     ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]
-    // ];
-// FourthLocation = 
-// [
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "leaf", "empty", "empty", "leaf", "leaf", "leaf", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "leaf", "leaf", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["leaf", "leaf", "wood", "leaf", "leaf", "empty", "wood", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "wood", "empty", "empty", "empty", "wood", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "wood", "empty", "empty", "empty", "wood", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-//     ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
-//     ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]
-// ];
-// FithLocation = 
-// [
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
-//     ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
-//     ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
-//     ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]
-// ]; 
+const FirstLocation =
+    [
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "leaf", "empty"],
+        ["empty", "empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty", "empty", "empty", "leaf", "leaf", "leaf", "leaf"],
+        ["empty", "empty", "empty", "empty", "leaf", "leaf", "leaf", "empty", "leaf", "empty", "leaf", "leaf", "leaf", "wood", "leaf"],
+        ["empty", "empty", "empty", "leaf", "leaf", "wood", "leaf", "leaf", "leaf", "leaf", "leaf", "leaf", "leaf", "wood", "leaf"],
+        ["empty", "empty", "leaf", "leaf", "wood", "wood", "leaf", "leaf", "wood", "leaf", "wood", "leaf", "leaf", "wood", "empty"],
+        ["empty", "empty", "empty", "empty", "wood", "wood", "empty", "empty", "wood", "empty", "wood", "empty", "empty", "wood", "empty"],
+        ["empty", "empty", "empty", "empty", "wood", "wood", "empty", "empty", "wood", "empty", "wood", "empty", "empty", "wood", "empty"],
+        ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]
+    ];
+const SecondLocation =    [
+        ["empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty", "empty", "empty", "leaf", "empty", "empty", "leaf", "empty"],
+        ["empty", "leaf", "empty", "leaf", "leaf", "leaf", "empty", "leaf", "empty", "leaf", "leaf", "leaf", "leaf", "leaf", "leaf"],
+        ["leaf", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "wood", "leaf"],
+        ["leaf", "wood", "leaf", "leaf", "wood", "leaf", "leaf", "wood", "leaf", "leaf", "wood", "leaf", "wood", "wood", "leaf"],
+        ["empty", "wood", "leaf", "wood", "wood", "leaf", "leaf", "wood", "wood", "leaf", "wood", "leaf", "wood", "wood", "empty"],
+        ["empty", "wood", "empty", "wood", "wood", "empty", "empty", "wood", "wood", "empty", "wood", "empty", "wood", "wood", "empty"],
+        ["empty", "wood", "empty", "wood", "wood", "empty", "empty", "wood", "wood", "empty", "wood", "empty", "wood", "wood", "empty"],
+        ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]  
+    ];
+const ThirdLocation =
+    [
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty"],
+        ["empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty", "empty", "leaf", "leaf", "leaf", "leaf", "empty", "empty"],
+        ["empty", "empty", "leaf", "leaf", "leaf", "leaf", "empty", "leaf", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "empty"],
+        ["empty", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "leaf", "leaf", "wood", "leaf", "wood", "leaf", "leaf", "empty"],
+        ["leaf", "leaf", "wood", "leaf", "wood", "wood", "leaf", "wood", "leaf", "wood", "empty", "wood", "wood", "leaf", "leaf"],
+        ["empty", "empty", "wood", "empty", "wood", "wood", "empty", "wood", "empty", "wood", "empty", "wood", "wood", "empty", "empty"],
+        ["empty", "empty", "wood", "empty", "wood", "wood", "empty", "wood", "empty", "wood", "empty", "wood", "wood", "empty", "empty"],
+        ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]
+    ];
+const FourthLocation = 
+    [
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "empty", "empty", "empty", "empty", "leaf", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "leaf", "empty", "empty", "leaf", "leaf", "leaf", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "leaf", "leaf", "leaf", "leaf", "leaf", "wood", "leaf", "leaf", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["leaf", "leaf", "wood", "leaf", "leaf", "empty", "wood", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "wood", "empty", "empty", "empty", "wood", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "wood", "empty", "empty", "empty", "wood", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]
+    ];
+const FithLocation = 
+    [
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"],
+        ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"],
+        ["ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground", "ground"]
+    ]; 
 
-
+const lvllr = [lvl1, FirstLocation, SecondLocation, ThirdLocation, FourthLocation, FithLocation];
 
 const tools = ["woodenAxe", "woodenPick", "stoneAxe", "stonePick", "ironAxe", "ironPick", "diamondAxe", "diamondPick"];
 
